e647b0bb3c94f5d1806a98c7d9b44ab43e4913b3
Mike Fajardo Turner <miguelstrife@gmail.com>
2025-06-24 22:11:47 -0600

    Added a toolset module to be shared between luas

---
+++ config/addons/LuAshitacast/common/toolset.lua
@@ -0,0 +1,38 @@
+local toolset = T{}
+
+toolset.ResetVariables = function(settings)
+    for key, value in pairs(settings) do
+        settings[key] = false;
+    end
+end
+
+toolset.SendMessageToChatBoxHelper = function(message)
+    AshitaCore:GetChatManager():QueueCommand(1, '/echo ' .. message);
+end
+
+toolset.CommandHandlerHelper = function(table, key)
+    local property = table[key];
+    if (property == true) then
+        AshitaCore:GetChatManager():QueueCommand(1, '/echo ===== [' .. key .. '] is now disabled. =====');
+        table[key] = false;
+    else
+        -- This is to have only one set triggered at a time.
+        toolset.ResetVariables();
+        table[key] = true;
+        AshitaCore:GetChatManager():QueueCommand(1, '/echo ===== [' .. key .. '] is now enabled. =====');
+    end
+end
+
+toolset.CompareStringsIgnoreCase = function(str1, str2)
+    return str1:lower() == str2:lower();
+end
+
+toolset.ShowSettings = function(settings)
+    local message = '===== LuAshitaCast Settings =====\n';
+    for key, value in pairs(settings) do
+        message = message .. key .. ': ' .. tostring(value) .. '\n';
+    end
+    toolset.SendMessageToChatBoxHelper(message);
+end
+
+return toolset;
